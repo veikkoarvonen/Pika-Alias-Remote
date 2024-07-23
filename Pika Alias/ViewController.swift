d5251778ac401488363ccc7fc5432ba97bf19c30
Veikko Arvonen <arvonenveikko@gmail.com>
2024-07-23 15:24:10 +0300

    Initial Commit

---
+++ Pika Alias/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pika Alias
+//
+//  Created by Veikko Arvonen on 23.7.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
