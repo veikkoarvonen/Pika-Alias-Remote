056278ee70f4bb89df5f648610010fefada2df66
Veikko Arvonen <arvonenveikko@gmail.com>
2024-07-31 15:55:12 +0300

    Start UI

---
+++ Pika Alias/Model/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Pika Alias
+//
+//  Created by Veikko Arvonen on 31.7.2024.
+//
+
+import UIKit
+
+struct C {
+    static let font = "EricaOne-Regular"
+    static let green = "green"
+
+}
